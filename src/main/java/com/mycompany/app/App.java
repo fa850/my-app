0d43966696917a27fefb0aab19a6b7107014c9c7
fa082281 <fa082281@W1744947.northamerica.cerner.net>
2020-07-23 11:18:22 -0500

    Commited

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Making a change" );
+        System.out.println( "Making a second change" );
     }
 }
